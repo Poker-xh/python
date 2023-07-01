e2af767a0ba67bbb64610f57468f35e1768e1894
pokerface <poker.xh.face@gmail.com>
2023-07-01 20:56:01 +0800

    go on with aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,9 +2,11 @@ import sys
 
 import pygame
 
+from pygame.sprite import Group
+
 from settings import Settings
 from ship import Ship
-
+import game_functions as gf
 def run_game():
     #初始化游戏并创建一个屏幕对象
     pygame.init()
@@ -13,21 +15,21 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
 
     #创建一艘飞船
-    ship=Ship(screen)
+    ship=Ship(ai_settings,screen)
     bg_color = (230,230,230)
+    bullets = Group()
     #开始游戏的主循环
     while True:
 
         #监视键盘和鼠标事件
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        gf.check_events(ai_settings,screen,ship,bullets)
         #每次循环都会重绘屏幕
-        screen.fill(ai_settings.bg_color)
-        ship.biltme()
-        #绘制屏幕可见
-        pygame.display.flip()
-
+         #绘制屏幕可见
+        gf.check_events(ai_settings,screen,ship,bullets)
+        ship.update()
+        gf.update_bullets(bullets)
+ 
+        gf.update_screen(ai_settings,screen,ship,bullets)
 
 
 run_game()
